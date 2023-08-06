ddbf6c9e59107395c1796c222ac7645ce94ca4a7
seongEun95 <dsa020701@gmail.com>
2023-08-06 11:24:45 +0900

    충돌 제거

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 let name = "kim seong eun";
 const name2 = "siwon";
 console.log(`hello world ${name}`);
-console.log(name2);
+let names = ["seong eun", "si won"];
+// console.log(names[0]);
+// console.log(names[1]);
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+names.forEach(function (v) {
+  console.log(v);
+});
+
+let newNames = names.map(function (v) {
+  return `kim ${v}`;
+});
+
+console.log(newNames);
